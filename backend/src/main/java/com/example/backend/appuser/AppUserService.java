263ed5fa8f99806b6f03e451a55686c563ea820a
Christian Weck <weck.chr@gmail.com>
2023-03-10 18:46:34 +0100

    Set up create endpoint for users

--- backend/src/main/java/com/example/backend/appuser/AppUserService.java
+++ backend/src/main/java/com/example/backend/appuser/AppUserService.java
@@ -1,7 +1,11 @@
 package com.example.backend.appuser;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
@@ -10,8 +14,50 @@ import java.util.Optional;
 public class AppUserService {
 
     private final AppUserRepository appUserRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public Optional<AppUser> findByUsername(String username) {
+
         return this.appUserRepository.findByUsername(username);
     }
+
+    public AppUser create(AppUser appUser) {
+
+        Optional<AppUser> existingUser = findByUsername(
+                appUser.getUsername()
+        );
+
+        if (existingUser.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT);
+        }
+
+        appUser.setPassword(passwordEncoder.encode(appUser.getPassword()));
+
+        if (
+                SecurityContextHolder
+                        .getContext()
+                        .getAuthentication() == null
+                        ||
+                !SecurityContextHolder.getContext()
+                        .getAuthentication()
+                        .isAuthenticated()
+                        ||
+                SecurityContextHolder
+                        .getContext()
+                        .getAuthentication()
+                        .getAuthorities()
+                        .stream()
+                        .noneMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ADMIN"))
+        ) {
+            appUser.setRole("BASIC");
+        }
+
+        this.appUserRepository.save(appUser);
+
+        appUser.setPassword("");
+
+        return appUser;
+    }
+
+    
 }
