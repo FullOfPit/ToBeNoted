f3d46cd9af711045fdf67773eb08fa116f2491d1
Christian Weck <weck.chr@gmail.com>
2023-03-10 14:22:27 +0100

    Added SpringSecurity Dependency; added AppUserService, Implemented findByUsername

--- backend/src/main/java/com/example/backend/appuser/AppUserController.java
+++ backend/src/main/java/com/example/backend/appuser/AppUserController.java
@@ -1,5 +1,7 @@
 package com.example.backend.appuser;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +10,15 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/app-users")
+@RequiredArgsConstructor
 public class AppUserController {
+
+    private final AppUserService appUserService;
 
     @PostMapping("/login")
     public Optional<AppUser> login() {
         return appUserService.findByUsername(
-                SecurityContextHolder
+                SecurityContextHolder.getContext().getAuthentication().getName()
         );
     }
 }
--- backend/src/main/java/com/example/backend/appuser/AppUserRepository.java
+++ backend/src/main/java/com/example/backend/appuser/AppUserRepository.java
@@ -3,8 +3,10 @@ package com.example.backend.appuser;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AppUserRepository extends MongoRepository<AppUser, String> {
 
-
+    Optional<AppUser> findByUsername(String username);
 }
