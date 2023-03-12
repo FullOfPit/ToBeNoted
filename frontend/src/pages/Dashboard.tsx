0697fac7c5b91ac9c7710237aab833485809673c
Christian Weck <weck.chr@gmail.com>
2023-03-12 19:32:14 +0100

    Copied two headerbar options from mui for testing

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,7 +1,11 @@
+import HeaderBar from "../components/HeaderBar";
+import HeaderBarTwo from "../components/HeaderBarTwo";
+
 export default function Dashboard() {
 
     return (
         <div>
+            <HeaderBar/>
             This de dashboard!
         </div>
     )
