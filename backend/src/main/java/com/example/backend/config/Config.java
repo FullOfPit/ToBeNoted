f1913174ee33a5b72372781b4b7e3c6f19ff5f36
Christian Weck <weck.chr@gmail.com>
2023-03-22 10:37:03 +0100

    Variability for the DeleteButton

--- backend/src/main/java/com/example/backend/config/Config.java
+++ backend/src/main/java/com/example/backend/config/Config.java
@@ -1,4 +1,8 @@
 /*
+
+//Deployment
+
+
 package com.example.backend.config;
 
 import org.springframework.context.annotation.Configuration;
--- frontend/src/components/buttons/DeleteButton.tsx
+++ frontend/src/components/buttons/DeleteButton.tsx
@@ -2,13 +2,21 @@ import {DeleteForeverRounded} from "@mui/icons-material";
 import {IconButton} from "@mui/material";
 import * as React from "react";
 
-export default function DeleteButton() {
+export default function DeleteButton(
+    {
+        id,
+        methodFunction,
+    } : {
+        id?: string,
+        methodFunction?: (id: string) => void
+    }) {
 
     return (
         <IconButton
             key={"EditButton"}
             sx={{padding: 1.5}}
             aria-label="round shadow"
+            onClick={ methodFunction && id? () => methodFunction(id) : undefined}
         >
             {<DeleteForeverRounded/>}
         </IconButton>
